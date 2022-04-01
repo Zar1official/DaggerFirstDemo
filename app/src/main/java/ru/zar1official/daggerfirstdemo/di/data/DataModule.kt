14448e302e4315f845a950b9bd324803ce31e8ce
zar1official <dimazar233445@gmail.com>
2022-04-01 13:56:41 +0300

    fixed packages

---
+++ app/src/main/java/ru/zar1official/daggerfirstdemo/di/data/DataModule.kt
@@ -0,0 +1,22 @@
+package ru.zar1official.daggerfirstdemo.di.data
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import ru.zar1official.daggerfirstdemo.data.RepositoryImpl
+import ru.zar1official.daggerfirstdemo.domain.Repository
+import javax.inject.Singleton
+
+@Module(includes = [StorageModule::class])
+object DataModule {
+    @Singleton
+    @Provides
+    fun provideRepository(repositoryImpl: RepositoryImpl): Repository =
+        repositoryImpl
+
+    @Singleton
+    @Provides
+    fun provideRepositoryImpl(dataStore: DataStore<Preferences>) =
+        RepositoryImpl(dataStore)
+}
